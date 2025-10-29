cf2ae69f2ad797945dca7dab1ae529cd4c732c3d
“Bharat-candescent” <nadigbharat7@gmail.com>
2025-10-29 21:47:03 +0530

    add

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = (env, argv) => {
   
   // Production (Vercel): Use the root-relative path
   // Assumes the deployment URL for this MFE is poc-webpack-repo1.vercel.app/
-  const prodPath = '/online-banking-mfe-1/';
+  // const prodPath = '/online-banking-mfe-1/';
+  const prodPath = '/';
 
   const publicPath = isProduction ? prodPath : devPath;
   
